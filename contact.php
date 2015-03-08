3d089f6f45977fe87f62978398882a75077df42a
Dan Forsberg <dforsber@gmail.com>
2015-03-08 19:32:43 +0200

    shadows

--- contact.php
+++ contact.php
@@ -1,48 +1,48 @@
-    </div>
+</div>
 
-    <!-- Page Content -->
-    <div class="container-fluid contact">
-        <div class="container">
-        <div class="row contact">
-            <div class="col-md-3">
-             <img src="images/dforsber.png" class="facepalm"><br>
-             <p>OTA YHTEYTTÄ</p>
-             ISECure Oy<br>
-             CEO Dr. Dan Forsberg, CSPO<br>
-             dan.forsberg (a) isecure.fi<br>
-             +358 40 483 5507<br>
-             <br>
-             Melkonkatu 7 A 33<br>
-             00210 Helsinki, Finland<br>
-             Y-tunnus: 2296336-9<br>
-            </div>
-            <div class="col-md-9">
-                <p> ISECure Oy on ohjelmistokehittäjä Dan Forsbergin
-                perustama yritys, joka tarjoaa Web Service
-                –pankkiyhteyden verifioitua koodia ja
-                räätälöintipalveluita yrityksille, järjestöille ja
-                yhdistyksille. Forsberg on toiminut
-                asiantuntijatehtävissä mobiiliverkostojen,
-                tietoturvan, telecomin, talouden ja
-                ohjelmistokehittämisen parissa. Hän johti
-                tutkimushankkeita Nokia Research Centerissä 10 vuoden
-                ajan, josta siirtyi Poplatek Oy:öön
-                kehitysjohtajaksi. Forsbergin erikoisosaamista ovat
-                EMV-systeemit, PCI DSS sekä PA DSS
-                –turvallisuussuunnittelu sekä AWS-pilvipalvelut.
-		</p>
-		<p class="contact">
-		  <input type="text" name="name" placeholder="Nimi"><br>
-		  <input type="text" name="email" placeholder="S&auml;hk&ouml;posti"><br>
-		  <input type="text" name="subject" placeholder="Otsikko"><br>
-		  <textarea name="viesti" cols="" rows="5" placeholder="Viesti"></textarea><br>
-                  <a href="#" class="btn btn-primary">L&Auml;HET&Auml;</a>
-		</p>
-            </div>
-        </div>
-        <!-- /.row -->
-        </div>
+<!-- Page Content -->
+<div class="container-fluid contact shadow">
+  <div class="container">
+    <div class="row contact">
+      <div class="col-md-3">
+        <img src="images/dforsber.png" class="facepalm"><br>
+        <p>OTA YHTEYTTÄ</p>
+        ISECure Oy<br>
+        CEO Dr. Dan Forsberg, CSPO<br>
+        dan.forsberg (a) isecure.fi<br>
+        +358 40 483 5507<br>
+        <br>
+        Melkonkatu 7 A 33<br>
+        00210 Helsinki, Finland<br>
+        Y-tunnus: 2296336-9<br>
+      </div>
+      <div class="col-md-9">
+        <p> ISECure Oy on ohjelmistokehittäjä Dan Forsbergin
+          perustama yritys, joka tarjoaa Web Service
+          –pankkiyhteyden verifioitua koodia ja
+          räätälöintipalveluita yrityksille, järjestöille ja
+          yhdistyksille. Forsberg on toiminut
+          asiantuntijatehtävissä mobiiliverkostojen,
+          tietoturvan, telecomin, talouden ja
+          ohjelmistokehittämisen parissa. Hän johti
+          tutkimushankkeita Nokia Research Centerissä 10 vuoden
+          ajan, josta siirtyi Poplatek Oy:öön
+          kehitysjohtajaksi. Forsbergin erikoisosaamista ovat
+          EMV-systeemit, PCI DSS sekä PA DSS
+          –turvallisuussuunnittelu sekä AWS-pilvipalvelut.
+	</p>
+	<p class="contact">
+	  <input type="text" name="name" placeholder="Nimi"><br>
+	  <input type="text" name="email" placeholder="S&auml;hk&ouml;posti"><br>
+	  <input type="text" name="subject" placeholder="Otsikko"><br>
+	  <textarea name="viesti" cols="" rows="5" placeholder="Viesti"></textarea><br>
+          <a href="#" class="btn btn-primary">L&Auml;HET&Auml;</a>
+	</p>
+      </div>
     </div>
+    <!-- /.row -->
+  </div>
+</div>
 
-    <!-- Page Content -->
-    <div class="container">
+<!-- Page Content -->
+<div class="container">
