4fa378617a775350ec4d9aea76f387dc62562536
Dan Forsberg <dforsber@gmail.com>
2015-03-08 18:52:50 +0200

    split index to frontpage.php

---
+++ frontpage.php
@@ -0,0 +1,70 @@
+<!-- Page Content -->
+<div class="container">
+  <div class="row">
+    <hr>
+  </div>
+  <div class="row well">
+    <div class="col-md-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">TUOTEKEHITT&Auml;JILLE<br>ISECure WS-Kanava</h3>
+        </div>
+        <div class="panel-body">
+          <p>VALMIS KOMPONENTTI PALVELU-KEHITTÄJILLE JA
+            OHJELMISTOTALOILLE</p>
+	  
+	  <p>Kun tarvitset liikeideaasi tai
+	    asiakasprojektiasi varten pankki-yhteyskanavan
+	    tai haluat automa-tisoida pankkiaineistojen
+	    käsittelyä.</p>
+	  
+	  <p>SELECTED / COLLABORATOR  +HOSTED</p>
+	  <img src="images/isecure_header1_b.png" style="width:100%;"><br><br>
+	  
+          <a href="#" class="btn btn-primary">VERTAA PAKETTEJA</a><br><br>
+          <a href="#" class="btn btn-primary">OTA YHTEYTT&Auml;</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">YRITYKSILLE<br>ISECure WS-Kustom</h3>
+        </div>
+        <div class="panel-body">
+	  <p>WS-KANAVAN INTEGROINTIPALVELUA,
+	    PANKKIAINEISTOJEN FORMAATTIMUUNNOKSIA JA MUUTA
+	    KONSULTOINTIA YRITYKSILLE</p>
+	  
+	  <p>Olet suunnittelemassa omaa palvelua ja
+	    tarvitset teknistä apua WS-Kanavan käytössä. Me
+	    osaamme pankkiyhteysasiat ja räätälöinnin.</p>
+	  
+	  <img src="images/isecure_ws1.jpg" style="width:100%;"><br><br>
+          <a href="#" class="btn btn-primary">OTA YHTEYTT&Auml;</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">J&Auml;RJEST&Ouml;ILLE<br>ISECure Ker&auml;ysStatus</h3>
+        </div>
+        <div class="panel-body">
+	  <p>VERKKOPALVELU HYVÄNTEKEVÄISYYS- JA
+	    VARAINKERÄYKSILLE</p>
+	  
+	  <p>KeräysStatus-palvelu tekee käteis- ja
+	    verkkomaksukeräyksistä helpompaa, hallitumpaa ja
+	    paremmin seurattavaa. Se rakentuu ISECuren
+	    WS-Kanavan päälle.</p>
+	  
+	  <img src="images/isecure_header_6.png" style="width:100%;"><br><br>
+          <a href="#" class="btn btn-primary">OTA YHTEYTT&Auml;</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.row -->
+</div>
+<!-- /.container -->
--- index.php
+++ index.php
@@ -1,107 +1,27 @@
 <?php include('header.php'); ?> 
 <body>
+  <?php include('navigation.php'); ?>
+  <?php include('carousel.php'); carousel(1); ?>
 
-    <!-- top navigation -->
-    <?php include('navigation.php'); ?>
-
-    <!-- Header Carousel -->
-    <?php include('carousel.php'); carousel(1); ?>
-
-    <!-- Page Content -->
-    <div class="container-fluid">
-        <div class="row supportedbanks">
-
-	  <div class="container">
-            <div class="col-md-12">
-              <!-- <img src="images/supported-banks.jpg" class="customer-img" style="width:100%;"> -->
-	      <p>TUETUT PANKIT: NORDEA, DANSKEBANK, OSUUSPANKKI, ÅLANDSBANKEN, S-PANKKI/LÄHITAPIOLA JA SAMLINK-PANKIT: <br>HANDELSBANKEN, AKTIA, POP, SÄÄSTÖPANKKI. VERKKOMAKSUNAPIT OSUUSPANKILLE, NORDEALLE JA DANSKEBANKILLE.</p>
-            </div>
-          </div>
-          <!-- /.row -->
-	</div>
-
-    </div>
-
-    <!-- Page Content -->
-    <div class="container">
-        <div class="row">
-            <hr>
+  <!-- Page Content -->
+  <div class="container-fluid">
+    <div class="row supportedbanks">
+      <div class="container">
+        <div class="col-md-12">
+          <!-- <img src="images/supported-banks.jpg" class="customer-img" style="width:100%;"> -->
+	  <p>TUETUT PANKIT: NORDEA, DANSKEBANK, OSUUSPANKKI, ÅLANDSBANKEN, S-PANKKI/LÄHITAPIOLA JA SAMLINK-PANKIT: <br>HANDELSBANKEN, AKTIA, POP, SÄÄSTÖPANKKI. VERKKOMAKSUNAPIT OSUUSPANKILLE, NORDEALLE JA DANSKEBANKILLE.</p>
         </div>
-        <div class="row well">
-            <div class="col-md-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">TUOTEKEHITT&Auml;JILLE<br>ISECure WS-Kanava</h3>
-                    </div>
-                    <div class="panel-body">
-                        <p>VALMIS KOMPONENTTI PALVELU-KEHITTÄJILLE JA
-                        OHJELMISTOTALOILLE</p>
- 
-			<p>Kun tarvitset liikeideaasi tai
-			asiakasprojektiasi varten pankki-yhteyskanavan
-			tai haluat automa-tisoida pankkiaineistojen
-			käsittelyä.</p>
-
-			<p>SELECTED / COLLABORATOR  +HOSTED</p>
-			<img src="images/isecure_header1_b.png" style="width:100%;"><br><br>
-
-                        <a href="#" class="btn btn-primary">VERTAA PAKETTEJA</a><br><br>
-                        <a href="#" class="btn btn-primary">OTA YHTEYTT&Auml;</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">YRITYKSILLE<br>ISECure WS-Kustom</h3>
-                    </div>
-                    <div class="panel-body">
-		      <p>WS-KANAVAN INTEGROINTIPALVELUA,
-		      PANKKIAINEISTOJEN FORMAATTIMUUNNOKSIA JA MUUTA
-		      KONSULTOINTIA YRITYKSILLE</p>
- 
-		      <p>Olet suunnittelemassa omaa palvelua ja
-		      tarvitset teknistä apua WS-Kanavan käytössä. Me
-		      osaamme pankkiyhteysasiat ja räätälöinnin.</p>
-
-		      <img src="images/isecure_ws1.jpg" style="width:100%;"><br><br>
-                      <a href="#" class="btn btn-primary">OTA YHTEYTT&Auml;</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">J&Auml;RJEST&Ouml;ILLE<br>ISECure Ker&auml;ysStatus</h3>
-                    </div>
-                    <div class="panel-body">
-		      <p>VERKKOPALVELU HYVÄNTEKEVÄISYYS- JA
-		      VARAINKERÄYKSILLE</p>
- 
-		      <p>KeräysStatus-palvelu tekee käteis- ja
-		      verkkomaksukeräyksistä helpompaa, hallitumpaa ja
-		      paremmin seurattavaa. Se rakentuu ISECuren
-		      WS-Kanavan päälle.</p>
-
-		      <img src="images/isecure_header_6.png" style="width:100%;"><br><br>
-                      <a href="#" class="btn btn-primary">OTA YHTEYTT&Auml;</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <?php include('contact.php'); ?>
-
-        <hr>
-        <?php include('ws-info.php'); ?>
-        <!-- Footer -->
-        <?php include('footer.php'); ?>
+      </div>
+      <!-- /.row -->
     </div>
-    <!-- /.container -->
+  </div>
 
-    <?php include('end.php'); dynamic(); ?>
+  <?php include('frontpage.php'); ?>
+  <?php include('contact.php'); ?>
+  <hr>
+  <?php include('ws-info.php'); ?>
+  <?php include('footer.php'); ?>
+  <?php include('end.php'); dynamic(); ?>
 
 </body>
-
 </html>
