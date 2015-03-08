1b346696e24eb680715219f2336375b72d067c3a
Dan Forsberg <dforsber@gmail.com>
2015-03-08 18:55:57 +0200

    cleanup index and header

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
-<?php include('header.php'); ?> 
-<body>
-  <?php include('navigation.php'); ?>
-  <?php include('carousel.php'); carousel(1); ?>
-
-  <!-- Page Content -->
-  <div class="container-fluid">
-    <div class="row supportedbanks">
-      <div class="container">
-        <div class="col-md-12">
-          <!-- <img src="images/supported-banks.jpg" class="customer-img" style="width:100%;"> -->
-	  <p>TUETUT PANKIT: NORDEA, DANSKEBANK, OSUUSPANKKI, ÅLANDSBANKEN, S-PANKKI/LÄHITAPIOLA JA SAMLINK-PANKIT: <br>HANDELSBANKEN, AKTIA, POP, SÄÄSTÖPANKKI. VERKKOMAKSUNAPIT OSUUSPANKILLE, NORDEALLE JA DANSKEBANKILLE.</p>
-        </div>
-      </div>
-      <!-- /.row -->
-    </div>
-  </div>
-
-  <?php include('frontpage.php'); ?>
-  <?php include('contact.php'); ?>
-  <hr>
-  <?php include('ws-info.php'); ?>
-  <?php include('footer.php'); ?>
-  <?php include('end.php'); dynamic(); ?>
-
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('header.php'); ?>
+  </head>
+  <body>
+    <?php include('navigation.php'); ?>
+    <?php include('carousel.php'); carousel(1); ?>
+    <?php include('supportedbanks-fluid.php'); ?>
+    <?php include('frontpage.php'); ?>
+    <?php include('contact.php'); ?>
+    <hr>
+    <?php include('ws-info.php'); ?>
+    <?php include('footer.php'); ?>
+    <?php include('end.php'); dynamic(); ?>
+  </body>
 </html>
