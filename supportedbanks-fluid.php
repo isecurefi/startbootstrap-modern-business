3e9df3c1e87fbed37a66d6b08f2607f6fbf56179
Dan Forsberg <dforsber@gmail.com>
2015-03-08 19:27:39 +0200

    add missing file

---
+++ supportedbanks-fluid.php
@@ -0,0 +1,17 @@
+<!-- Page Content -->
+<div class="container-fluid">
+  <div class="row supportedbanks">
+    <div class="container">
+      <div class="col-md-12">
+        <!-- <img src="images/supported-banks.jpg" class="customer-img" style="width:100%;"> -->
+	<p>TUETUT PANKIT: NORDEA, DANSKEBANK, OSUUSPANKKI,
+	ÅLANDSBANKEN, S-PANKKI/LÄHITAPIOLA JA
+	SAMLINK-PANKIT: <br>HANDELSBANKEN, AKTIA, POP,
+	SÄÄSTÖPANKKI. VERKKOMAKSUNAPIT OSUUSPANKILLE, NORDEALLE JA
+	DANSKEBANKILLE.</p>
+      </div>
+    </div>
+    <!-- /.row -->
+  </div>
+</div>
+
