b9e5f54df56adce283f118d128dc1120c034608c
Dan Forsberg <dforsber@gmail.com>
2015-03-15 17:52:32 +0200

    footer with xs

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
           </div>
           <hr>
           <div class="row">
-            <div class="col-xs-4 footer-col">
+            <div class="col-xs-12 col-sm-4 footer-col">
 	    <img src="images/isecure-small-logo.png" style="width:80%;"><br>
 	    <p><a href="https://www.isecure.fi/">https://www.isecure.fi/</a></p>
             </div>
-            <div class="col-xs-4 footer-col">
+            <div class="col-xs-6 col-sm-4 footer-col">
 	      <p>
 		CEO Dr. Dan Forsberg, CSPO<br>
 		<b>ISECure Oy</b><br>
@@ -16,7 +16,7 @@
 		+358 40 483 5507
 	      </p>
             </div>
-            <div class="col-xs-4 footer-col">
+            <div class="col-xs-6 col-sm-4 footer-col">
 	      <p>
 		Helsinki, Finland<br>
 		Y-tunnus: 2296336-9<br>
