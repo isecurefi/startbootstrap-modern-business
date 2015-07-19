01bc2b9cc07e27c063005ce6443b130300c69fc4
Dan Forsberg <dforsber@gmail.com>
2015-07-19 15:17:22 +0300

    bank icons

---
+++ banks.php
@@ -0,0 +1,17 @@
+<div class="row">
+  <div class="col-md-12">
+    <br>
+    <center>
+      <img src="images/bank_icons/aktia_logo.png">&nbsp;&nbsp;
+      <img src="images/bank_icons/alandsbanken_logo.gif">&nbsp;&nbsp;
+      <img src="images/bank_icons/handelsbanken_logo.gif">&nbsp;&nbsp;
+      <img src="images/bank_icons/nooa_logo.gif">&nbsp;&nbsp;
+      <img src="images/bank_icons/nordea_logo.gif">&nbsp;&nbsp;
+      <img src="images/bank_icons/oko_logo.gif">&nbsp;&nbsp;
+      <img src="images/bank_icons/pop_logo.gif">&nbsp;&nbsp;
+      <img src="images/bank_icons/sampo_logo.gif">&nbsp;&nbsp;
+      <img src="images/bank_icons/spankki_logo.gif">&nbsp;&nbsp;
+      <img src="images/bank_icons/tapiola_logo.gif">&nbsp;&nbsp;
+    </center>
+  </div>
+</div>
